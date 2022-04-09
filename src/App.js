296d8b912a2ca3226b3adb52d78d169d14034eab
abi <abimael.monarrez58@gmail.com>
2022-04-09 00:57:27 -0600

    create functions to change page and send props to Navigation

--- src/App.js
+++ src/App.js
@@ -3,23 +3,36 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Navigation from './components/Navigation';
 import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
 import { useState } from 'react';
 
 function App() {
-  let [page, setPage] = useState('About');
+  let [page, setPage] = useState('About Me');
 
-  function handlePage() {
-    if (page === 'About') {
-      return <About />
+  function changePage (pageName) {
+    console.log(pageName);
+    setPage(pageName);
+}
+
+  function renderPage() {
+    switch(page)
+    {
+      case 'Portfolio' : return <Portfolio />
+      case 'Contact' : return <Contact />
+      case 'Resume' : return <Resume />
+      default : return <About />
     }
   }
+
   return (
     <div className='content'>
       <header>
         <Header />
-        <Navigation />
+        <Navigation setPage={setPage} changePage={changePage}/>
       </header>
-      {handlePage()}
+      {renderPage()}
       <Footer />
     </div>
   )
