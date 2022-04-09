0845d077b79c9edffebb7d7ddcda0a3b0629a4af
abi <abimael.monarrez58@gmail.com>
2022-04-08 18:29:28 -0600

    return a list of the sections on the nav bar

---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function hello() {
+    return (
+        <ul>
+            <li>About Me</li>
+            <li>Portfolio</li>
+            <li>Contact</li>
+            <li>Resume</li>
+        </ul>
+    )
+}
+
+export default hello;
