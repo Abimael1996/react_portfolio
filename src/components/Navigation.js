f9a777c0c37a7c25fc4bc90a2b8e15bbf253cdbf
abi <abimael.monarrez58@gmail.com>
2022-04-09 02:05:30 -0600

    add the class 'highlighted' to the About Me title so it's highlighted on load

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,10 +4,10 @@ function Navigation(props) {
     return (
         <nav>
             <ul>
-                <li onClick={(e) => props.changePage(e.target.innerHTML)}>About Me</li>
-                <li onClick={(e) => props.changePage(e.target.innerHTML)}>Portfolio</li>
-                <li onClick={(e) => props.changePage(e.target.innerHTML)}>Contact</li>
-                <li onClick={(e) => props.changePage(e.target.innerHTML)}>Resume</li>
+                <li onClick={(e) => props.changePage(e.target)} className='highlighted'>About Me</li>
+                <li onClick={(e) => props.changePage(e.target)}>Portfolio</li>
+                <li onClick={(e) => props.changePage(e.target)}>Contact</li>
+                <li onClick={(e) => props.changePage(e.target)}>Resume</li>
             </ul>
         </nav>
     )
