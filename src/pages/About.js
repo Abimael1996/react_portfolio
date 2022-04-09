6e80cc9efe76a412b19b55a161e89879ecac33f0
abi <abimael.monarrez58@gmail.com>
2022-04-08 23:20:45 -0600

    change what was in Project.js and place it on About.js

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function About() {
+    return (
+        <div>
+            <h2>About Me</h2>
+            <img src='./abimael.jpg' alt="Abimael"></img>
+            <p>Hello! I am a Front-end Web Developer leveraging an Entertainment Business background to build an
+engaging user experience on websites related to the Entertainment Industry. <br/><br/>Known as a
+creative problem solver looking to apply my skills as part of a fast-paced, innovative and
+quality-driven team to build better and more creative experiences on the web.</p>
+        </div>
+    )
+}
+
+export default About;
