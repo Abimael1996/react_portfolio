f80d8270eebed7476a39ecdf8c4429e80fba1ab9
abi <abimael.monarrez58@gmail.com>
2022-04-10 14:57:45 -0600

    return a download resume link and a list of proficiencies

---
+++ src/pages/Resume.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function Resume() {
+    return (
+        <main className='mainContent'>
+            <p>Download my <span><a href='./Abimael-Monarrez-Resume.pdf' target='_blank' download>resume</a></span></p>
+            <h3>Front-end Proficiencies</h3>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>jQuery</li>
+                <li>responsive design</li>
+                <li>React</li>
+                <li>Bootstrap</li>
+            </ul>
+            <h3>Back-end Proficiencies</h3>
+            <ul>
+                <li>APIs</li>
+                <li>Node</li>
+                <li>Express</li>
+                <li>MySQL, Sequelize</li>
+                <li>MongoDB, Mongoose</li>
+                <li>REST</li>
+                <li>GraphQL</li>
+            </ul>
+        </main>
+    )
+}
+
+export default Resume;
