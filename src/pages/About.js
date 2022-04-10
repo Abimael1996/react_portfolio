9a7e2d8f2ca9d6a2d6b360d9a2965edf030cd0bd
abi <abimael.monarrez58@gmail.com>
2022-04-09 19:56:28 -0600

    change <div> for <main>

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React from 'react';
 
 function About() {
     return (
-        <div>
-            <img src='./abimael.jpg' alt="Abimael"></img>
+        <main className='mainContent'>
+            <img src='./images/abimael.jpg' alt="Abimael"></img>
             <p>Hello! I am a Front-end Web Developer leveraging an Entertainment Business background to build an
 engaging user experience on websites related to the Entertainment Industry. <br/><br/>Known as a
 creative problem solver looking to apply my skills as part of a fast-paced, innovative and
 quality-driven team to build better and more creative experiences on the web.</p>
-        </div>
+        </main>
     )
 }
 
