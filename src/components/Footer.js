baf33cefc971515270ce870fadfcc90eab03d9bd
abi <abimael.monarrez58@gmail.com>
2022-04-12 04:37:40 -0600

    add basic style to Portfolio page and modify icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,10 @@ import { faGithub, faLinkedin, faStackOverflow } from '@fortawesome/free-brands-
 function Footer() {
     return (
         <footer className='mt-auto align-self-center'>
-            <div className='icons'>
-                <a href='https://github.com/Abimael1996' target='_blank' rel='noreferrer'><FontAwesomeIcon icon={faGithub} /></a>
-                <a href='https://www.linkedin.com/in/abimael-monarrez/' target='_blank' rel='noreferrer'><FontAwesomeIcon icon={faLinkedin} /></a>
-                <FontAwesomeIcon icon={faStackOverflow} />
+            <div>
+                <a href='https://github.com/Abimael1996' target='_blank' rel='noreferrer'><FontAwesomeIcon icon={faGithub} className='icons-footer'/></a>
+                <a href='https://www.linkedin.com/in/abimael-monarrez/' target='_blank' rel='noreferrer'><FontAwesomeIcon icon={faLinkedin} className='icons-footer'/></a>
+                <FontAwesomeIcon icon={faStackOverflow} className='icons-footer'/>
             </div>
         </footer>
     )
