738b5d15a8b074bcc92d080b350068b158e41838
abi <abimael.monarrez58@gmail.com>
2022-04-12 04:35:57 -0600

    make the Portfolio page responsive

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,17 +5,15 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 function Project(props) {
 
     return (
-        <div className='projectContainer'>
-            <img src={props.imgLink} alt='perfect meal app' className='projectImg'/>
-            <div className='projectContent hide'>
-                <div className='links'>
-                    <a href={props.appLink} target='_blank' rel='noreferrer'><h3 className='appLink'>{props.title}</h3></a>
-                    <a href={props.gHlink} target='_blank' rel='noreferrer' className='gHLink'><FontAwesomeIcon icon={faGithub} /></a>
+        <div className='project-container'>
+            <div className='content-container'>
+                <div className='w-100 px-3 d-flex justify-content-between links'>
+                    <a href={props.appLink} target='_blank' rel='noreferrer' className='text-decoration-none text-reset'><h3 className='display-4'>{props.title}</h3></a>
+                    <a href={props.gHlink} target='_blank' rel='noreferrer' className='gHLink'><FontAwesomeIcon icon={faGithub} className='icons-links'/></a>
                 </div>
-                <div>
-                    <h4>{props.description}</h4>
-                    <p>{props.technologies}</p>
-                </div>
+                <p className='px-3 technologies'>{props.technologies}</p>
+                <a href={props.appLink} target='_blank' rel='noreferrer'><img src={props.imgLink} alt='perfect meal app' className='col-12 project-img' /></a>
+                <h5 className='px-3 description'>{props.description}</h5>
             </div>
         </div>
     )
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -9,7 +9,7 @@ function Portfolio() {
             gHlink: 'https://github.com/Abimael1996/Perfect_Meal',
             appLink: 'https://thawing-depths-19417.herokuapp.com/',
             description: 'Perfect Meal is a web app that makes nutritionist work easier.',
-            technologies: 'Sequelize, Handlebars, Express, JavaScript, CSS, HTML',
+            technologies: 'Sequelize | Handlebars | Express | JavaScript | CSS | HTML',
             id: 1
         },
         {
@@ -18,7 +18,7 @@ function Portfolio() {
             gHlink: 'https://github.com/Abimael1996/weather_dashboard',
             appLink: 'https://abimael1996.github.io/weather_dashboard/',
             description: 'This web application allows the user to search for any city and obtain information about its weather.',
-            technologies: 'Weather API, JavaScript, CSS, HTML',
+            technologies: 'Weather API | JavaScript | CSS | HTML',
             id: 2
         },
         {
@@ -27,7 +27,7 @@ function Portfolio() {
             gHlink: 'https://github.com/Abimael1996/work_day_scheduler',
             appLink: 'https://abimael1996.github.io/work_day_scheduler/',
             description: 'This project consists of a calendar application that allows a user to save events for each hour of a working schedule (9 to 5).',
-            technologies: 'jQuery, JavaScript, CSS, HTML',
+            technologies: 'jQuery | JavaScript | CSS | HTML',
             id: 3
         },
         {
@@ -36,7 +36,7 @@ function Portfolio() {
             gHlink: 'https://github.com/Abimael1996/tech_blog',
             appLink: 'https://sheltered-reaches-16118.herokuapp.com/',
             description: 'The Tech Blog is an internet forum for developers to discuss about tech.',
-            technologies: 'Sequelize, Handlebars, Express, JavaScript, CSS, HTML',
+            technologies: 'Sequelize | Handlebars | Express | JavaScript | CSS | HTML',
             id: 4
         },
         {
@@ -45,7 +45,7 @@ function Portfolio() {
             gHlink: 'https://github.com/Abimael1996/text_editor',
             appLink: 'https://peaceful-headland-90409.herokuapp.com/',
             description: 'A PWA Application that allows you to create notes or code snippets with or without an internet connection.',
-            technologies: 'Webpack, Workbox, JavaScript, CSS, HTML',
+            technologies: 'Webpack | Workbox | JavaScript | CSS | HTML',
             id: 5
         },
         {
@@ -54,15 +54,15 @@ function Portfolio() {
             gHlink: 'https://github.com/rargotte/watch-chill',
             appLink: 'https://rargotte.github.io/watch-chill/',
             description: 'A web app that recommends movies based on criteria selected by the user.',
-            technologies: 'The Movie DB API, JavaScript, CSS, HTML',
+            technologies: 'The Movie DB API | JavaScript | Bulma | CSS | HTML',
             id: 6
         },
     ];
 
     return (
-        <main id='portfolio' className='mainContent'>
+        <main className='mb-4 d-flex flex-column flex-sm-row flex-sm-wrap justify-content-sm-around'>
             {projects.map((project) => (
-                <section key={project.id} className='project'>
+                <section key={project.id} className='col-12 col-sm-5 mb-4'>
                     <Project
                         imgLink={project.imgLink}
                         title={project.title}
