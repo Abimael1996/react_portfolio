c64f9c81935cc577cdb0e03ac3b8537e92a34742
abi <abimael.monarrez58@gmail.com>
2022-04-13 03:22:45 -0600

    make the Contact page responsive

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 function Contact() {
     let [name, setName] = useState('');
@@ -8,7 +10,7 @@ function Contact() {
     let [alert, setAlert] = useState('');
 
     function validateEmail(emailAdress) {
-         const regexEmail = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        const regexEmail = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
         if (emailAdress.match(regexEmail)) {
             return true;
         } else {
@@ -72,44 +74,52 @@ function Contact() {
         <main className='mainContent'>
             <div className='contact'>
                 <form>
-                    <label>Name:</label>
-                    <input
-                        type='text'
-                        value={name}
-                        name='name'
-                        onChange={handleChange}
-                    />
-                    <label>Email address:</label>
-                    <input
-                        type='text'
-                        value={email}
-                        name='email'
-                        onChange={handleChange}
-                        onClick={(e) => validate(e.target.localName)}
-                    />
-                    <label>Message:</label>
-                    <textarea
-                        value={message}
-                        rows='10'
-                        cols='80'
-                        name='message'
-                        onChange={handleChange}
-                        onClick={(e) => validate(e.target.localName)}>
-                    </textarea>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" className="form-label">Name</label>
+                        <input
+                            type="text"
+                            class="form-control"
+                            id="exampleFormControlInput1"
+                            name='name' value={name}
+                            onChange={handleChange} />
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" className="form-label">Email address</label>
+                        <input
+                            type="email"
+                            class="form-control"
+                            id="exampleFormControlInput1"
+                            name='email' value={email} onChange={handleChange}
+                            onClick={(e) => validate(e.target.localName)} />
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                        <textarea
+                            class="form-control"
+                            id="exampleFormControlTextarea1"
+                            rows="3" name='message'
+                            value={message}
+                            onChange={handleChange}
+                            onClick={(e) => validate(e.target.localName)}></textarea>
+                    </div>
                     <p className={
                         displayAlert
                             ? ''
                             : 'hide'
                     }>{alert}</p>
-                    <button onClick={(e) => {
+                    <button type="submit" class="btn btn-primary" onClick={(e) => {
                         e.preventDefault();
                         validate(e.target.localName)
-                    }
-                    }>Submit</button>
+                    }}>Submit</button>
                 </form>
-                <div className='contactLinks'>
-                    <a href='mailto:abimael.monarrez58@gmail.com'><h3>abimael.monarrez58@gmail.com</h3></a>
-                    <a href='tel:52-667-420-3849'><h3>+56 667 420 3849</h3></a>
+                <div className='row justify-content-center'>
+                    <div className="card d-flex flex-column align-items-center my-3 p-2 col-10 col-lg-4">
+                        <h5 className="card-title">Contact Info</h5>
+                        <div className='d-flex flex-column'>
+                            <a href="mailto:abimael.monarrez58@gmail.com" className='mb-3'><FontAwesomeIcon icon={faEnvelope} className='me-2'/>abimael.monarrez58@gmail.com</a>
+                            <a href="tel:52-667-420-3849"><FontAwesomeIcon icon={faPhone} className='me-2'/>+56 667 420 3849</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </main>
