08e4e4277ed72e51c1937a3bd0c13b88f030562c
abi <abimael.monarrez58@gmail.com>
2022-04-14 21:46:10 -0600

    make the Resume page responsive

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -3,27 +3,33 @@ import React from 'react';
 function Resume() {
     return (
         <main className='ms-4'>
-            <p>Download my full <span><a href='./Abimael-Monarrez-Resume.pdf' target='_blank' download>resume</a></span></p>
-            <h3>Front-end Proficiencies</h3>
-            <ul>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JavaScript</li>
-                <li>jQuery</li>
-                <li>responsive design</li>
-                <li>React</li>
-                <li>Bootstrap</li>
-            </ul>
-            <h3>Back-end Proficiencies</h3>
-            <ul>
-                <li>APIs</li>
-                <li>Node</li>
-                <li>Express</li>
-                <li>MySQL, Sequelize</li>
-                <li>MongoDB, Mongoose</li>
-                <li>REST</li>
-                <li>GraphQL</li>
-            </ul>
+            <p className='fs-1'>Download my full <span><a href='./Abimael-Monarrez-Resume.pdf' target='_blank' download>resume</a></span></p>
+            <div className='d-flex flex-column justify-content-start flex-sm-row justify-content-sm-around'>
+                <div>
+                    <h3 className='display-6'>Front-end Proficiencies</h3>
+                    <ul className='fs-3'>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JavaScript</li>
+                        <li>jQuery</li>
+                        <li>responsive design</li>
+                        <li>React</li>
+                        <li>Bootstrap</li>
+                    </ul>
+                </div>
+                <div>
+                    <h3 className='display-6'>Back-end Proficiencies</h3>
+                    <ul className='fs-3'>
+                        <li>APIs</li>
+                        <li>Node</li>
+                        <li>Express</li>
+                        <li>MySQL, Sequelize</li>
+                        <li>MongoDB, Mongoose</li>
+                        <li>REST</li>
+                        <li>GraphQL</li>
+                    </ul>
+                </div>
+            </div>
         </main>
     )
 }
