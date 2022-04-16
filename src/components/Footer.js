3df4f48a93f3b138ffd9dec66ccd4a38d9506cd8
abi <abimael.monarrez58@gmail.com>
2022-04-15 20:49:25 -0600

    add Stack Overflow link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
             <div className='d-flex justify-content-around'>
                 <a href='https://github.com/Abimael1996' target='_blank' rel='noreferrer' className='text-reset'><FontAwesomeIcon icon={faGithub} className='icons-footer'/></a>
                 <a href='https://www.linkedin.com/in/abimael-monarrez/' target='_blank' rel='noreferrer' className='text-reset'><FontAwesomeIcon icon={faLinkedin} className='icons-footer'/></a>
-                <FontAwesomeIcon icon={faStackOverflow} className='icons-footer'/>
+                <a href='https://stackoverflow.com/users/18820271/abi' target='_blank' rel='noreferrer' className='text-reset'><FontAwesomeIcon icon={faStackOverflow} className='icons-footer'/></a>
             </div>
         </footer>
     )
