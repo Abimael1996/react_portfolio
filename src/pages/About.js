478d8c253b46a01e4adf132b9a14d71a68214b08
abi <abimael.monarrez58@gmail.com>
2022-05-21 14:11:55 -0600

    remove dash

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,7 @@ function About() {
     return (
         <main className='mx-4 mb-4 d-flex flex-column flex-sm-row justify-content-around'>
             <img src='./images/abimael.jpg' alt="Abimael" className='mb-4 rounded-circle w-25 align-self-center profile-pic'></img>
-            <p className='fs-5 col-12 col-sm-6 m-auto'>Hello! I am a Full-stack Web Developer leveraging an Entertainment Business background to build an
+            <p className='fs-5 col-12 col-sm-6 m-auto'>Hello! I am a Full Stack Web Developer leveraging an Entertainment Business background to build an
 engaging user experience on websites related to the Entertainment Industry. <br/><br/>Known as a
 creative problem solver looking to apply my skills as part of a fast-paced, innovative and
 quality-driven team to build better and more creative experiences on the web.</p>
